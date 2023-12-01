c7b9e52b647bf071f193e131e610d1991395417c
najeira <najeira@gmail.com>
2023-12-01 23:06:48 +0900

    init

---
+++ lib/src/navigation_listener.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/widgets.dart';
+
+/// A widget that listens to navigation events.
+class NavigationListener extends StatefulWidget {
+  const NavigationListener({
+    super.key,
+    required this.child,
+    this.onPopNext,
+    this.onPush,
+    this.onPop,
+    this.onPushNext,
+  });
+
+  /// The widget below this widget in the tree.
+  final Widget child;
+
+  /// Called when the top route has been popped off, and the current route
+  /// shows up.
+  final VoidCallback? onPopNext;
+
+  /// Called when the current route has been pushed.
+  final VoidCallback? onPush;
+
+  /// Called when the current route has been popped off.
+  final VoidCallback? onPop;
+
+  /// Called when a new route has been pushed, and the current route is no
+  /// longer visible.
+  final VoidCallback? onPushNext;
+
+  @override
+  State<NavigationListener> createState() => NavigationListenerState();
+}
+
+class NavigationListenerState extends State<NavigationListener>
+    with RouteAware {
+  RouteObserver<ModalRoute<dynamic>>? _observer;
+
+  @override
+  @mustCallSuper
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _subscribe();
+  }
+
+  void _subscribe() {
+    final observer = _findRouteObserver(context);
+    if (observer != _observer) {
+      _unsubscribe();
+    }
+    _observer = observer;
+
+    final route = ModalRoute.of(context);
+    if (route != null) {
+      observer?.subscribe(this, route);
+    }
+  }
+
+  @override
+  @mustCallSuper
+  void dispose() {
+    _unsubscribe();
+    super.dispose();
+  }
+
+  void _unsubscribe() {
+    _observer?.unsubscribe(this);
+    _observer = null;
+  }
+
+  @override
+  void didPopNext() {
+    widget.onPopNext?.call();
+  }
+
+  @override
+  void didPush() {
+    widget.onPush?.call();
+  }
+
+  @override
+  void didPop() {
+    widget.onPop?.call();
+  }
+
+  @override
+  void didPushNext() {
+    widget.onPushNext?.call();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+}
+
+RouteObserver<ModalRoute<dynamic>>? _findRouteObserver(BuildContext context) {
+  final nav = Navigator.maybeOf(context);
+  if (nav != null) {
+    for (final observer in nav.widget.observers) {
+      if (observer is RouteObserver<ModalRoute<dynamic>>) {
+        return observer;
+      }
+    }
+  }
+  return null;
+}
---
+++ test/navigation_listener_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:navigation_listener/navigation_listener.dart';
+
+void main() {
+  test('new', () {
+    NavigationListener(child: Container());
+  });
+}
